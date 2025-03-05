7e4e7c1298cc7c853cbd362f6127c0f483cdca28
SY17900 <2332681550@qq.com>
2025-03-05 16:23:39 +0800

    strings ok

---
+++ binaryTree/basis.go
@@ -0,0 +1,77 @@
+package binarytree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (t *TreeNode) PreOrder() (res []int) {
+	if t == nil {
+		return
+	}
+
+	stack := []*TreeNode{}
+	stack = append(stack, t)
+	for len(stack) != 0 {
+		cur := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, cur.Val)
+		if cur.Right != nil {
+			stack = append(stack, cur.Right)
+		}
+		if cur.Left != nil {
+			stack = append(stack, cur.Left)
+		}
+	}
+
+	return
+}
+
+func (t *TreeNode) InOrder() (res []int) {
+	if t == nil {
+		return
+	}
+
+	stack := []*TreeNode{}
+	cur := t
+	for cur != nil || len(stack) != 0 {
+		for cur != nil {
+			stack = append(stack, cur)
+			cur = cur.Left
+		}
+		cur = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, cur.Val)
+		stack = append(stack, cur.Right)
+	}
+
+	return
+}
+
+func (t *TreeNode) PostOrder() (res []int) {
+	if t == nil {
+		return
+	}
+
+	stack := []*TreeNode{}
+	cur := t
+	var pre *TreeNode
+	for cur != nil || len(stack) != 0 {
+		for cur != nil {
+			stack = append(stack, cur)
+			cur = cur.Left
+		}
+		cur = stack[len(stack)-1]
+		if cur.Right == nil || cur.Right == pre {
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+			pre = cur
+			cur = nil
+		} else {
+			cur = cur.Right
+		}
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"mygo/sorting"
+	"mygo/strings"
 )
 
 func main() {
-	nums := []int{5, 2, 4, 6, 1, 3}
-	sorting.HeapSort(nums)
-	for index, num := range nums {
-		fmt.Print(num)
-		if index != len(nums)-1 {
-			fmt.Print(" ")
-		}
-	}
-	fmt.Println()
+	str := []byte("abcdefg")
+	strings.MoveRight(str, 2)
+	var res string = string(str)
+	fmt.Println(res)
 }
