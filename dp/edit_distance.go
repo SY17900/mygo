4c4bb5848296da35c1eb23471bbc41e4797080a3
SY17900 <2332681550@qq.com>
2025-03-17 21:09:50 +0800

    dp ing & SHOPEE

--- dp/edit_distance.go
+++ dp/edit_distance.go
@@ -1,18 +1,65 @@
 package dp
 
-// func IsSubsequence(s, t string) bool {
-// 	dp := make([][]int, len(s)+1)
-// 	for i := range dp {
-// 		dp[i] = make([]int, len(t))
-// 	}
+func IsSubsequence(s, t string) bool {
+	dp := make([][]int, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(t)+1)
+	}
 
-// 	for i := 1; i <= len(s); i++ {
-// 		for j := 1; j <= len(t); j++ {
-// 			if s[i-1] == t[j-1] {
-// 				dp[i][j] = dp[i-1][j-1] + 1
-// 			} else {
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(t); j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
 
-// 			}
-// 		}
-// 	}
-// }
+	return dp[len(s)][len(t)] == len(s)
+}
+
+func NumOfSubsequences(s, t string) int {
+	dp := make([][]int, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(t)+1)
+		dp[i][0] = 1
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(t); j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[len(s)][len(t)]
+}
+
+func MinDistance(word1, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(word2)+1)
+	}
+	for i := range dp {
+		dp[i][0] = i
+	}
+	for j := range dp[0] {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= len(word1); i++ {
+		for j := 1; j <= len(word2); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j-1]+2, min(dp[i-1][j]+1, dp[i][j-1]+1))
+			}
+		}
+	}
+
+	return dp[len(word1)][len(word2)]
+}
--- exam/SHOPEE_0317.go
+++ exam/SHOPEE_0317.go
@@ -1,1 +1,128 @@
 package exam
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func SHOPEE_EXAM1() {
+	var str string
+	fmt.Scanln(&str)
+
+	startIndex, ans := 0, 1
+	rec := make(map[byte]int)
+	for i := range str {
+		if dIndex, ok := rec[str[i]]; ok {
+			ans = max(ans, i-startIndex)
+			for j := startIndex; j <= dIndex; j++ {
+				delete(rec, str[j])
+			}
+			rec[str[i]] = i
+			startIndex = dIndex + 1
+			continue
+		}
+		rec[str[i]] = i
+	}
+	ans = max(ans, len(str)-startIndex)
+
+	fmt.Println(ans)
+}
+
+func SHOPEE_EXAM2() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	temp := sc.Text()
+
+	num1 := make([]int, 0)
+	num2 := make([]int, 0)
+	fs, fe := strings.IndexByte(temp, '['), strings.IndexByte(temp, ']')
+	ss, se := strings.LastIndexByte(temp, '['), strings.LastIndexByte(temp, ']')
+	tempS1 := strings.Split(temp[fs+1:fe], ",")
+	tempS2 := strings.Split(temp[ss+1:se], ",")
+	for _, str := range tempS1 {
+		num, _ := strconv.Atoi(str)
+		num1 = append(num1, num)
+	}
+	for _, str := range tempS2 {
+		num, _ := strconv.Atoi(str)
+		num2 = append(num2, num)
+	}
+
+	ans := make([]int, len(num1)+len(num2))
+	index := len(ans) - 1
+	var i, j int
+	for i, j = len(num1)-1, len(num2)-1; i >= 0 && j >= 0; {
+		if num1[i] >= num2[j] {
+			ans[index] = num1[i]
+			i--
+		} else {
+			ans[index] = num2[j]
+			j--
+		}
+		index--
+	}
+
+	if i >= 0 {
+		for m := 0; m <= i; m++ {
+			ans[m] = num1[m]
+		}
+	}
+	if j >= 0 {
+		for m := 0; m <= j; m++ {
+			ans[m] = num2[m]
+		}
+	}
+
+	fmt.Print("[")
+	for i := range ans {
+		fmt.Print(ans[i])
+		if i != len(ans)-1 {
+			fmt.Print(",")
+		}
+	}
+	fmt.Println("]")
+}
+
+func SHOPEE_EXAM3() {
+	var str string
+	var k int
+	fmt.Scan(&str, &k)
+
+	ifValid := func(chr byte) bool {
+		if chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u' {
+			return true
+		}
+		return false
+	}
+
+	ans, rec := 0, 0
+	for i := range k {
+		if ifValid(str[i]) {
+			rec++
+		}
+	}
+	ans = rec
+	if ans == k {
+		fmt.Println(ans)
+		return
+	}
+
+	for i := k; i < len(str); i++ {
+		if ifValid(str[i-k]) {
+			rec--
+		}
+		if ifValid(str[i]) {
+			rec++
+		}
+		ans = max(ans, rec)
+		if ans == k {
+			fmt.Println(ans)
+			return
+		}
+	}
+
+	fmt.Println(ans)
+}
